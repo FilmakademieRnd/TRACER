05e0ed107a396f0dbf8300ea6bab6f210aac167b
Simon Spielmann <sspielma@filmakademie.de>
2024-07-10 11:10:53 +0200

    - update header

--- Core/Managers/SceneManager/SceneManager.cs
+++ Core/Managers/SceneManager/SceneManager.cs
@@ -1,20 +1,21 @@
-/*
-TRACER FOUNDATION - 
+/* 
+TRACER FOUNDATION -
 Toolset for Realtime Animation, Collaboration & Extended Reality
-tracer.research.animationsinstitut.de
+
+Copyright (c) 2024 Filmakademie Baden-Wuerttemberg, Animationsinstitut R&D Labs
+https://research.animationsinstitut.de/tracer 
 https://github.com/FilmakademieRnd/TRACER
 
-Copyright (c) 2023 Filmakademie Baden-Wuerttemberg, Animationsinstitut R&D Lab
-
-TRACER is a development by Filmakademie Baden-Wuerttemberg, Animationsinstitut
-R&D Labs in the scope of the EU funded project MAX-R (101070072) and funding on
-the own behalf of Filmakademie Baden-Wuerttemberg.  Former EU projects Dreamspace
-(610005) and SAUCE (780470) have inspired the TRACER development.
+TRACER FOUNDATION is a development by Filmakademie Baden-Wuerttemberg,
+Animationsinstitut R&D Labs in the scope of the EU funded project
+MAX-R (101070072) and funding on the own behalf of Filmakademie Baden-Wuerttemberg.
+Former EU projects Dreamspace (610005) and SAUCE (780470) have inspired the
+TRACER FOUNDATION development.
 
 This program is distributed in the hope that it will be useful, but WITHOUT
 ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
 FOR A PARTICULAR PURPOSE. See the MIT License for more details.
-You should have received a copy of the MIT License along with this program; 
+You should have received a copy of the MIT License along with this program;
 if not go to https://opensource.org/licenses/MIT
 */
 
